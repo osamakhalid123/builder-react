f5d9193799b95735b06fd06240ba7cf7db26b28b
osamakhalid123 <osamakhalid797@gmail.com>
2022-08-02 22:27:05 +0200

    port

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 // export const API_HOST = "http://localhost:8080/api/";
-export const API_HOST = "https://page-builder-node.vercel.app/";
+export const API_HOST = "https://pagebuilder13.herokuapp.com/api/";
