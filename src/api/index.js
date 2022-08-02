17b22c9ab8286be53581b84fcf26d0d748441302
osamakhalid123 <osamakhalid797@gmail.com>
2022-08-02 17:40:33 +0200

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,2 @@
-export const API_HOST = "http://localhost:8080/api/";
-// export const API_HOST = "https://git.heroku.com/pagebuildervwn1.git";
-// export const API_HOST = " https://node-vwn.herokuapp.com/";
+// export const API_HOST = "http://localhost:8080/api/";
+export const API_HOST = "https://page-builder-node.vercel.app/";
