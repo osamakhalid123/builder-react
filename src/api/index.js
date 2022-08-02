308868bfb03339e0df63d3bcba80a6cfcc065980
osamakhalid123 <osamakhalid797@gmail.com>
2022-08-02 22:48:53 +0200

    post-14

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,3 @@
 // export const API_HOST = "http://localhost:8080/api/";
-export const API_HOST = "https://pagebuilder13.herokuapp.com/api/";
+// export const API_HOST = "https://pagebuilder13.herokuapp.com/api/";
+export const API_HOST = "https://pagebuilder-git.herokuapp.com/api/";
