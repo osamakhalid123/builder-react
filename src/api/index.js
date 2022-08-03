688e410f005b8462df6d1e438e131e3bb4eca12c
osamakhalid123 <osamakhalid797@gmail.com>
2022-08-03 19:48:29 +0200

    api V5

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,2 @@
 // export const API_HOST = "http://localhost:8080/api/";
-export const API_HOST = "https://builder-vwn.herokuapp.com/api/";
+export const API_HOST = "https://builder-vwn.herokuapp.com";
